cbf4f843432ca83b180e24022265108548e97f1a
Wayne Hong <woo.young.hong@hotmail.com>
2018-10-03 16:37:05 +0900

    updated .zip file to successfully upload to azure website

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world!!!!!');
 });
 app.listen(PORT, () => {
   console.log('port is listening at ', PORT);
